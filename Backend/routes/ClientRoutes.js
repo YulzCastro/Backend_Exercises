ef046edd9a598c2914c688229e1c6f70275bee4f
YulzCastro <andreayulitzacastrogomez88@gmail.com>
2023-08-16 18:34:44 -0500

    Client part

--- Backend/routes/ClientRoutes.js
+++ Backend/routes/ClientRoutes.js
@@ -1,9 +1,18 @@
-const express = require('express');
+/* const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>  res.status(200).send({
     message:"Client"
 }));
+
+module.exports = router; */
+
+const express = require('express');
+const router = express.Router();
+//const ClientController = require('./../controllers/ClientController');
+const { ClientController } = require('./../controllers');
+
+router.get('/', ClientController.getClients);
 
 module.exports = router;
 
--- Backend/routes/ProductRoutes.js
+++ Backend/routes/ProductRoutes.js
@@ -6,3 +6,4 @@ router.get('/', (req, res) =>  res.status(200).send({
 }));
 
 module.exports = router;
+
