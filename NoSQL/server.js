da2136ab351a9bdfc2742cb0e636a54a3fb7efeb
YulzCastro <andreayulitzacastrogomez88@gmail.com>
2023-08-30 21:34:08 -0500

    Starting NoSQL

---
+++ NoSQL/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = 3000
+
+const server = express()
+
+server.use(express.json())
+
+server.use
+server.listen(PORT, () => {
+    console.log(`Escuchando en el puerto ${PORT}`)
+})
