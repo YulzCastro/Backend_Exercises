f1b7c953deb2a866fb3332d3e10c31f497b156e4
YulzCastro <andreayulitzacastrogomez88@gmail.com>
2023-09-04 21:21:14 -0500

    NoSQL

--- NoSQL/server.js
+++ NoSQL/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const TaskRoutes = require('./routes/TaskRoutes')
 const PORT = 3000
 
 const server = express()
 
 server.use(express.json())
 
-server.use
+server.use('/api/v1/tasks', TaskRoutes)
+
+const mongooseConnect = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://andreayulitzacastrogomez88:ZLgGJbGJKsjonAmC@cluster0.gb4wkof.mongodb.net/')
+        console.log('Conexión exitosa')
+    }catch (error){
+        console.error(error)
+    }
+}
+
+mongooseConnect()
+
 server.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${PORT}`)
 })
